93ad758e1b9e64178b8544d6e792ccf0996c3b21
Nicholas <nicholas@basepaint.dev>
2024-03-11 00:56:21 +0000

    update configuration

---
+++ db/seed.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import bcrypt from "bcrypt";
+
+async function main() {
+  const users = [
+    {
+      name: "User",
+      email: "user@nextmail.com",
+      password: "123456",
+    },
+    {
+      name: "Admin",
+      email: "admin@testmail.com",
+      password: "654321",
+    },
+  ];
+
+  // Seed Users
+  users.map(async (user) => {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    const response = await prisma.user.upsert({
+      where: { email: user.email },
+      update: {},
+      create: {
+        email: user.email,
+        name: user.name,
+        password: hashedPassword,
+      },
+    });
+    console.log(response);
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
